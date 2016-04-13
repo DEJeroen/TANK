7cab605361536ed9244b97d365f2e8f3492fc151
jeroen <jeroenrietveld482@gmail.com>
2016-04-13 10:15:54 +0200

    Added bluetooth class

---
+++ Android/Megalania/app/src/main/java/com/tank/megalania/BluetoothConnection.java
@@ -0,0 +1,152 @@
+package com.tank.megalania;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.support.annotation.MainThread;
+import android.util.Log;
+import android.util.Xml;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.UUID;
+
+/**
+ * Created by DEJeroen on 2/26/16.
+ */
+
+
+public class BluetoothConnection {
+
+
+    private OutputStream mOutputStream;
+    private InputStream mInputStream;
+    private BluetoothAdapter mBluetoothAdapter;
+    BluetoothSocket socket;
+    BluetoothDevice device = null;
+    final String macAddress = "70:F3:95:29:0A:CE";
+    ArrayList<BluetoothDevice> pairedDeviceArrayList;
+    boolean connected = false;
+    byte[] received = new byte[128];
+
+
+    //Enable emulated RS-232 serial port via Bluetooth
+    public void connect() throws BluetoothConnectionException {
+
+        // Assign the Bluetooth adapter to mBluetoothAdapter
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        //If Bluetooth adapter does not exists throw exception
+        if (mBluetoothAdapter == null)
+            throw new BluetoothConnectionException("Device does not support Bluetooth");
+
+        // Check if Bluetooth is disabled
+        if (!mBluetoothAdapter.isEnabled()) {
+            throw new BluetoothConnectionException("Bluetooth is disabled");
+        }
+
+        new Thread(new Runnable() {
+            @Override
+            public void run(){
+                //Device to connect with
+                device = mBluetoothAdapter.getRemoteDevice(macAddress);
+                if(connected)Log.e("Oxygen Bluetooth","already connected");
+
+
+                try {
+                    if(!connected) {
+                        socket = device.createRfcommSocketToServiceRecord(UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"));
+                        socket.connect();
+                        Log.e("Oxygen Bluetooth", "connected");
+                    }
+                    mOutputStream = socket.getOutputStream();
+                    connected = true;
+                }catch (IOException e){
+                    Log.e("Oxygen Bluetooth", "error" + e);
+                }
+
+                try {
+                    if(connected)
+                    {
+
+                        while(true)
+                        {
+                            mInputStream = socket.getInputStream();
+                            mInputStream.read(received, 0, received.length);
+                            try {
+                                ShowData(received);
+                            }
+                            catch (IOException | BluetoothConnection.BluetoothConnectionException e) {
+                                Log.e("Oxygen Bluetooth", "exception: " + e);
+                            }
+
+                        }
+                    }
+                }
+                catch (IOException e)
+                {
+                    Log.e("Oxygen Bluetooth", "Connection Lost" + e);
+                    connected = false;
+
+                }
+            }}).start();
+    }
+
+    public void ShowData(byte[] message)throws BluetoothConnectionException, IOException{
+        if (mOutputStream == null)
+            throw new BluetoothConnectionException("Socket not connected");
+        String textmessage = new String(message, "US-ASCII");
+        String sProper = textmessage.replace("\0", "");
+
+        Log.v("Ontvangen", sProper);
+
+
+
+    }
+
+    public void send(byte[] data) throws BluetoothConnectionException, IOException {
+        if (mOutputStream == null)
+            throw new BluetoothConnectionException("Socket not connected");
+
+        mOutputStream.write(data, 0, data.length);
+    }
+
+
+    //Define Bluetooth Exception
+    public static class BluetoothConnectionException extends Exception {
+        public BluetoothConnectionException(String message) {
+            super(message);
+        }
+    }
+
+    public ArrayList<BluetoothDevice> getDevices() throws BluetoothConnectionException {
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        if (pairedDevices.size() > 0) {
+            pairedDeviceArrayList = new ArrayList<BluetoothDevice>();
+
+            for (BluetoothDevice device : pairedDevices) {
+                pairedDeviceArrayList.add(device);
+            }
+
+
+        }
+        return pairedDeviceArrayList;
+
+    }
+}
+
+
+
+
+
--- Android/Megalania/app/src/main/java/com/tank/megalania/MainActivity.java
+++ Android/Megalania/app/src/main/java/com/tank/megalania/MainActivity.java
@@ -1,13 +1,92 @@
 package com.tank.megalania;
 
+import android.bluetooth.BluetoothDevice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static int REQUEST_ENABLE_BT = 1;
+    BluetoothConnection btcon = new BluetoothConnection();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void onConnectClick(View view){
+        Log.e("Oxygen Bluetooth", "Trying to connect ");
+
+
+        try {
+            ArrayList<BluetoothDevice> pairedDeviceArrayList;
+            if(btcon!=null){
+
+                pairedDeviceArrayList = btcon.getDevices();
+            }
+            //   pairedDeviceArrayList = btcon.getDevices();
+            //  mac = btcon.getDevices().get(1).getAddress() ;
+            btcon.connect();
+
+        } catch (Exception ex) {
+            Log.e("Oxygen Bluetooth", "exception: " + ex);
+
+            //write BluetoothConnectionException to log
+            //  Log.e("Oxygen Bluetooth", "exception: " + e);
+
+            //if bluetooth is disabled ask the user to enable it
+            //   if (e.getMessage() == "Bluetooth is disabled"){
+            //      Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            //      startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+        // }
+    }
+
+    public void onLeftClick(View view){
+        try {
+            btcon.send("L".toString().getBytes());
+            btcon.send("255".toString().getBytes());
+            //mOutputView.setText("");
+
+        } catch (IOException | BluetoothConnection.BluetoothConnectionException e) {
+            Log.e("Oxygen Bluetooth", "exception: " + e);
+        }
+    }
+    public void onRightClick(View view){
+        try {
+            btcon.send("R".toString().getBytes());
+            btcon.send("255".toString().getBytes());
+            //mOutputView.setText("");
+
+        } catch (IOException | BluetoothConnection.BluetoothConnectionException e) {
+            Log.e("Oxygen Bluetooth", "exception: " + e);
+        }
+    }
+    public void onForwardClick(View view){
+        try {
+            btcon.send("F".toString().getBytes());
+            btcon.send("255".toString().getBytes());
+            //mOutputView.setText("");
+
+        } catch (IOException | BluetoothConnection.BluetoothConnectionException e) {
+            Log.e("Oxygen Bluetooth", "exception: " + e);
+        }
+    }
+    public void onBackwardClick(View view){
+        try {
+            btcon.send("B".toString().getBytes());
+            btcon.send("255".toString().getBytes());
+            //mOutputView.setText("");
+
+        } catch (IOException | BluetoothConnection.BluetoothConnectionException e) {
+            Log.e("Oxygen Bluetooth", "exception: " + e);
+        }
     }
 }
