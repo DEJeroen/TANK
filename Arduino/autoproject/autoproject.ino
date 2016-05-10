306cc20efffdf39c1caabd794fadf1b77e66eb52
DEJeroen <jeroenrietveld482@gmail.com>
2016-05-10 17:29:46 +0200

    Set TANK to send current speed to other car

--- Arduino/autoproject/autoproject.ino
+++ Arduino/autoproject/autoproject.ino
@@ -1,4 +1,4 @@
-rint motor_left[] = {4, 5};
+int motor_left[] = {4, 5};
 int motor_right[] = {2, 3};
 int i;                                                                            //store motor left, right pins
 int leftF, leftB, rightF, rightB;                                                 //variables for different motor states
@@ -30,15 +30,10 @@ void loop() {
   if (Serial1.available()) {
         inbyte = Serial1.read();
 
-        
-
-         if(inbyte ==  'r'){
+        //Reset (Here for when TANK is waiting for the input for the second motor)
+ if(inbyte ==  'r'){
           counter = 0;
-
-
-          }
-       
-        
+          }  
         if(inbyte >= '0' & inbyte <= '9')
             serialDataIn += inbyte - 48;
         if (inbyte == ','){  // Handle delimiter
@@ -83,13 +78,44 @@ void loop() {
             
             
           }
-          
+          //Reset wich motor is waiting for data
           counter = 0;
+
+          //Set motor to driveSpeed
           analogWrite(motor_left[0], driveSpeed[0]);
           analogWrite(motor_left[1], driveSpeed[1]); 
           analogWrite(motor_right[0], driveSpeed[2]);  
           analogWrite(motor_right[1], driveSpeed[3]);
 
+          //Set Antwerp car to new speed
+
+          if (driveSpeed[0] == 0 && driveSpeed[1] == 0  && driveSpeed[2] == 0 && driveSpeed[3]  )  {
+    //stop the car
+    Serial.println("stop");
+    Serial2.write("0");
+  }
+  else if  (driveSpeed[0] > 200 && driveSpeed[2] == 200 )  {
+    //drive car forward
+    Serial.println("forward");
+    Serial2.write("1");
+  }
+
+  else if  (driveSpeed[0] > 200 && driveSpeed[3] == 200)  {
+    //turn left fast
+     Serial2.write("3");
+  }
+
+  else if  (driveSpeed[1] > 200 && driveSpeed[2] == 200)  {
+    //turn right fast
+    Serial.println("fast right");
+    Serial2.write("5");
+  }
+  else if  (driveSpeed[3] > 200 && driveSpeed[4] == 200 )  {
+    //drive car backwards
+    Serial.println("backwards");
+    Serial2.write("6");
+  }
+          
           
 
        
